18c379bc68e13aae3589ddaa9c25cce8abb7a789
Shubham Grover <37283484+shubham-grover@users.noreply.github.com>
2019-05-22 15:33:46 +0530

    Add files via upload

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Transform Player;
+	public Text ScoreText;
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(Player.position.z);
+		ScoreText.text = Player.position.z.ToString("0");
+	}
+}
